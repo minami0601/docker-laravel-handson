78640b0e8e063c6d3e99cfd4ca5a6eb0b2445af7
minami0601 <minami.love.bb@icloud.com>
2021-01-29 16:48:12 +0900

    font-awsome install

--- backend/resources/lang/en/validation.php
+++ backend/resources/lang/en/validation.php
@@ -146,6 +146,11 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'name'=>'名前', //追記
+        'email'=>'メールアドレス', //追記
+        'password'=>'パスワード', //追記
+        'self_introduction'=>'自己紹介文', //追記
+    ],
 
 ];
